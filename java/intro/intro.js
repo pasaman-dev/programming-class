00d8a1193946afdaa1d90b6826a1e77f4ade1853
habibi <nurhabibim858@gmail.com>
2022-10-04 05:52:29 +0700

    js intro

---
+++ java/intro/intro.js
@@ -0,0 +1,6 @@
+// menukar element html
+document.getElementById("demo").innerHTML="hello world";
+// menukar style(css)
+document.getElementById("demo").style.fontSize="35px";
+// menukar display sytle
+document.getElementById("demo").style.display="none";
