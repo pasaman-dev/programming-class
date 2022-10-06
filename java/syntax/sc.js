f6defa09fb512ffede0066d199d44c46c50b09c4
habibi <nurhabibim858@gmail.com>
2022-10-07 05:56:49 +0700

    js syntax

---
+++ java/syntax/sc.js
@@ -0,0 +1,18 @@
+// angka bisa ditulis desimal atau tidak
+10.10
+1010
+// string bisa dengan ""atau''
+"habibi"
+'habibi'
+// js menggunakan var,let,dan const untuk deklarasi variabel
+var a;
+let b;
+a=10;
+b=11;
+// operator js (+*/-)
+a+b
+// = untuk mengisi value 
+let c;
+c=a+b;
+// komen menggunakan // atau /* */
+// js bersifat casesensitive
