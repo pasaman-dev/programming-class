253d6a8c392f8f8f854c7e70addb8963db517ee6
habibi <nurhabibim858@gmail.com>
2022-10-08 05:44:47 +0700

    js variable

---
+++ java/var/sc.js
@@ -0,0 +1,13 @@
+// deklarasi variabel bisa menggunakan let,var,const,atau tanpa apa apa
+var x=6;
+let y=7
+const z=x+y;
+a=10;
+// bisa deklarasi banyak variabel di 1 statement
+var a=3,b=4,c=car;
+// var bisa di deklarasi berulang dan nilai nya takkan hilang tapi tidak dengan let atau 
+var car=mobil;
+var car;
+// nama dengan $ dan _ adalah valid variable
+let $l=0;
+let _n=1;
