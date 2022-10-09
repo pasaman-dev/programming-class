bdb8238a7d2bd85dfb2d286e927eb8ca766c8ffb
habibi <nurhabibim858@gmail.com>
2022-10-10 05:47:31 +0700

    js operator

---
+++ java/operator/sc.js
@@ -0,0 +1,11 @@
+// = untuk mengisi variable
+let a=1;
+// aritmarika seperti +,*,-,/ juga bisa dipakai di js
+var b=7+8;
+// asignment operator seperi =+,=* juga bisa dipakai di js
+var c;
+c +=5;
+// + juga bisa untuk menambah string
+let d="manusia";
+let e="laki laki";
+let f=d+""+e;
