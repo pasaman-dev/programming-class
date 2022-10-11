defc5a7ba484c6022f253eded33974b889728a88
habibi <nurhabibim858@gmail.com>
2022-10-12 05:44:04 +0700

    js arithmetics

---
+++ java/aritmatika/sc.js
@@ -0,0 +1,22 @@
+// + untuk menambah
+let a=1+2;
+// - untuk mengurang
+let b=1-2;
+// * untuk kali
+let c=1*2;
+// / untuk bagi
+let d=1/2;
+// % untuk modulus atau sisa bagi
+let e=1%2;
+// untuk increment atau tambah 1
+let f=2;
+f++;
+// untuk decrement atau kurang 1
+let g=2;
+g++;
+// ** untuk memangkatkan bilangan pertama dengan bilangan kedua
+let h=2;
+h**2;
+// ** memiliki fungsi yang sama dengan Math.pow(a,b)
+let i=2;
+Math.pow(i,2);
