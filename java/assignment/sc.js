cba8e30a3e34e704f8195c4c779e4409c40a2090
habibi <nurhabibim858@gmail.com>
2022-10-12 05:54:33 +0700

    js assignment

---
+++ java/assignment/sc.js
@@ -0,0 +1,23 @@
+// = operator untuk menetapkan sebuah nilai
+let a=1;
+// += untuk menambahkan nilai ke variable
+let b=1;
+b+=2;
+// untuk mengurangi nilai ke variable
+let c=2;
+c-=1;
+// untuk mengali nilai ke variable
+let d=2;
+d*=3;
+// untuk membagi nilai ke variable
+let e=2;
+e/=4;
+// untuk modulus atau sisa bagi
+let f=2;
+e%=5;
+// &= untuk operator ands
+let g=2;
+g&=5;
+// != untuk operator or
+let h=2;
+h!=5;;
